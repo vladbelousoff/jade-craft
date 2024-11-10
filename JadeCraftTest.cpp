e2e28467567e5e3510cf6a0172b3caf158dcf59c
Vladislav Belousov <vladbelousov@yahoo.com>
2024-11-10 00:15:57 +0000

    More tests for TaskQueue

--- JadeCraftTest.cpp
+++ JadeCraftTest.cpp
@@ -25,3 +25,35 @@ TEST_CASE("TaskQueue Works in General", "[TaskQueue]")
       REQUIRE(numbers[i] == i);
    }
 }
+
+TEST_CASE("TaskQueue is thread-safe", "[TaskQueue][threaded]")
+{
+   jade::TaskQueue queue;
+   std::atomic<int> counter = 0;
+   constexpr int n = 50;
+
+   // Add tasks in multiple threads
+   std::thread t1([&queue, &counter] {
+      for (int i = 0; i < n; ++i) {
+         queue.add_task([&counter] {
+            counter++;
+         });
+      }
+   });
+
+   std::thread t2([&queue, &counter] {
+      for (int i = 0; i < n; ++i) {
+         queue.add_task([&counter] {
+            counter++;
+         });
+      }
+   });
+
+   t1.join();
+   t2.join();
+
+   // Perform tasks in a single thread
+   queue.perform_all_tasks();
+
+   REQUIRE(counter == n * 2);
+}
