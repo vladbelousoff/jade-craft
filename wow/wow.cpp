b89767b45e68a5625359fb5c3dfb1025c864881b
Vladislav Belousov <vladbelousov@yahoo.com>
2024-11-13 12:38:09 +0000

    Use SDL2 instead of GLFW to support DX9 later

--- wow/wow.cpp
+++ wow/wow.cpp
@@ -1,5 +1,6 @@
 #include <GL/gl3w.h>
-#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_main.h>
 
 #include <spdlog/spdlog.h>
 
@@ -32,65 +33,72 @@ main(int argc, char* argv[])
       wow::global->mpq_file_manager = std::make_unique<wow::MPQFileManager>(std::filesystem::current_path());
    }
 
-   if (!glfwInit()) {
-      spdlog::error("Failed to initialize GLFW!");
+   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+      spdlog::error("Failed to initialize SDL2: {}", SDL_GetError());
       return -1;
    }
 
-   jade::ScopeExit terminate_glfw([]() {
-      spdlog::info("Terminating GLFW...");
-      glfwTerminate();
+   jade::ScopeExit terminate_sdl([] {
+      spdlog::info("Terminating SDL2...");
+      SDL_Quit();
    });
 
-   glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+   SDL_Window* window = SDL_CreateWindow(
+       "WoW", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
-   glfwSetErrorCallback([](int error, const char* description) {
-      spdlog::error("Error code: 0x{:x}", error);
-      spdlog::error("Error description: {}", description);
-   });
-
-   // Create a windowed mode window and its OpenGL context
-   GLFWwindow* window = glfwCreateWindow(1920, 1080, "WoW", nullptr, nullptr);
    if (!window) {
-      spdlog::error("Failed to create a GLFW window!");
+      spdlog::error("Failed to create an SDL2 window: {}", SDL_GetError());
       return -1;
    }
 
-   jade::ScopeExit terminate_window([window]() {
-      spdlog::info("Terminating the window...");
-      glfwDestroyWindow(window);
+   jade::ScopeExit terminate_window([window] {
+      spdlog::info("Destroying window...");
+      SDL_DestroyWindow(window);
    });
 
-   // Make the window's context current
-   glfwMakeContextCurrent(window);
+   SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+   if (!gl_context) {
+      spdlog::error("Failed to create OpenGL context: {}", SDL_GetError());
+      return -1;
+   }
 
-   // VSYNC
-   glfwSwapInterval(1);
+   jade::ScopeExit terminate_gl_context([&] {
+      spdlog::info("Destroying OpenGL context...");
+      SDL_GL_DeleteContext(gl_context);
+   });
+
+   // Enable VSYNC
+   SDL_GL_SetSwapInterval(1);
 
    if (gl3wInit()) {
       spdlog::error("Failed to initialize gl3w!");
       return -1;
    }
 
-   // Main loop
-   while (!glfwWindowShouldClose(window)) {
-      glfwPollEvents();
+   bool running = true;
+   SDL_Event event;
+
+   while (running) {
+      while (SDL_PollEvent(&event)) {
+         if (event.type == SDL_QUIT) {
+            running = false;
+         }
+      }
 
       int display_w;
       int display_h;
-      glfwGetFramebufferSize(window, &display_w, &display_h);
+      SDL_GL_GetDrawableSize(window, &display_w, &display_h);
       glViewport(0, 0, display_w, display_h);
 
-      // Basic render
+      // Render
       glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-      glfwSwapBuffers(window);
+      SDL_GL_SwapWindow(window);
    }
 
    return 0;
