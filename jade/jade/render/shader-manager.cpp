6ffb34e5d4f06544cc9995d4142e7d5799f667b6
Vladislav Belousov <vladbelousov@yahoo.com>
2024-12-23 19:41:51 +0000

    Basic implementation of ShaderManager

---
+++ jade/jade/render/shader-manager.cpp
@@ -0,0 +1,30 @@
+#include "shader-manager.hpp"
+
+#include <spdlog/spdlog.h>
+
+auto
+jade::ShaderManager::create_shader(ShaderType, const void*) -> ShaderId
+{
+  generations.push_back(0);
+  return ShaderId(next_id++, 0);
+}
+
+void
+jade::ShaderManager::delete_shader(ShaderId shader_id)
+{
+  if (is_valid(shader_id)) {
+    generations[shader_id.unique_id.id - 1]++;
+  } else {
+    spdlog::warn("Attempted to delete a shader with id {}, which is not valid", shader_id.unique_id.id);
+  }
+}
+
+auto
+jade::ShaderManager::is_valid(ShaderId shader_id) const -> bool
+{
+  if (shader_id.unique_id.id > 0 && shader_id.unique_id.id <= generations.size()) {
+    return generations[shader_id.unique_id.id - 1] == shader_id.unique_id.generation;
+  }
+
+  return false;
+}
---
+++ jade/jade/render/shader-manager.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <vector>
+
+namespace jade {
+
+  enum class ShaderType
+  {
+    Vertex,
+    Fragment,
+  };
+
+  class ShaderManager;
+
+  class ShaderId
+  {
+    friend class ShaderManager;
+
+  public:
+    ShaderId() = default;
+
+    explicit ShaderId(std::uint32_t id, std::uint32_t generation)
+      : unique_id(id, generation)
+    {
+    }
+
+    bool operator==(const ShaderId& other) const
+    {
+      return unique_id.id == other.unique_id.id && unique_id.generation == other.unique_id.generation;
+    }
+
+    bool operator!=(const ShaderId& other) const
+    {
+      return !(*this == other);
+    }
+
+  private:
+    struct UniqueId
+    {
+      std::uint32_t id = 0;
+      std::uint32_t generation = 0;
+
+      UniqueId() = default;
+
+      UniqueId(std::uint32_t id, std::uint32_t generation)
+        : id(id)
+        , generation(generation)
+      {
+      }
+    };
+
+    UniqueId unique_id;
+  };
+
+  class ShaderManager
+  {
+  public:
+    auto create_shader(ShaderType type, const void* buffer) -> ShaderId;
+    void delete_shader(ShaderId shader_id);
+    auto is_valid(ShaderId shader_id) const -> bool;
+
+  private:
+    std::vector<std::uint32_t> generations;
+    std::uint32_t next_id = 1;
+  };
+
+} // namespace jade
